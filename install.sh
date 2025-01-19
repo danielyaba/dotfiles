4fa73a7e26866812777ac57a5ad3d6fc4081722b
Daniel Yaba <daniely@digital.gov.il>
2025-01-19 18:16:04 +0200

    feat: added install.sh for automation

---
+++ install.sh
@@ -0,0 +1,30 @@
+# install zsh
+brew install zsh
+
+# install wezterm terminal
+brew install --cask wezterm
+
+# install bat for better cat
+brew install bat
+
+# install jq for parsing JSON files
+brew instal jq
+
+# install yq for parsing YAML files
+brew install yq
+
+# install nerd-fonst
+brew tap homebrew/cask-fonts
+brew install font-meslo-lg-nerd-font
+
+# install neovim
+brew install neovim
+
+# install eza for better ls
+brew install eza
+
+# install zoxide for cd shortcuts 
+brew install zoxide
+
+# install helm
+brew install helm
