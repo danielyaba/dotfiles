1b6be5ac44d6e325eeb64d026e546522c85765c7
Daniel Yaba <daniely@digital.gov.il>
2025-02-09 10:18:35 +0200

    feat(tmux-aliases): combined tmux kill-server with fzf for convinience

--- zsh/aliases/tmux_alises.zsh
+++ zsh/aliases/tmux_alises.zsh
@@ -1,6 +1,6 @@
 ### TMUX ALIASES ###
 alias td='tmux detach'
 alias ta='tmux attach-session -t $(tmux list-sessions -F "#S" | fzf --preview="" --prompt="Select a tmux session: " --height=15 --border --reverse)'
-alias tk='tmux kill-session -t'
+alias tk='tmux kill-session -t $(tmux list-sessions -F "#S" | fzf --preview="" --prompt="Select a tmux session: " --height=15 --border --reverse)'
 alias tl='tmux ls'
 alias tns='tmux new -s'
